d341213126dc574d30c82a0aef108f8d64e9e716
Pradipkumar Desai <pdesai.programming@gmail.com>
2026-06-21 07:48:53 +0530

    flask api for ask

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from langchain_openai import ChatOpenAI
-
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def main():
-    llm =  ChatOpenAI(model="gpt-5",temperature=0)
-    response = llm.invoke("What is Spec driven development?")
-    print(response.content)
+app = Flask(__name__)
 
+llm =  ChatOpenAI(model="gpt-5",temperature=0)
+
+@app.route("/ask", methods=["POST"])
+def ask():   
+    data = request.get_json()
+    prompt = data.get("prompt")
+    if not prompt:
+        return jsonify({"error":"Invalid prompt"}), 400
+    print(f"prompt - {prompt}")
+    
+    response = llm.invoke(prompt)
+    
+    return jsonify({
+        "answer":response.content
+    })
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
